bb9bd8aa6abd1c46577a765ae59d0e1bb1b7f981
HieA1lpha <engkauanrodrigues@gmail.com>
2024-06-10 02:43:17 -0300

    2024-06-10

--- index.js
+++ index.js
@@ -5,23 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
     //recria o conteúdo com <span> e adiciona quebra de linha a cada quatro palavras.
     let formattedText = '';
     for (let i = 0; i < words.length; i++) {
-        formattedText += `<span>${words[i]}</span>`;
-        if ((i + 1) % 4 === 0 && i !== words.length) {
-            formattedText += '-';
+        let wordSpan = `<span>${words[i]}</span>`;
+        if ((i + 1) % 4 === 0 && i !== words.length - 1) {
+            wordSpan += '-';
         } if ((i + 1) % 8 === 0 && i !== words.length - 1) {
-            formattedText += '<br>';
+            wordSpan += '<br>';
         } else {
-            formattedText += ' ';
+            wordSpan += ' ';
         }
+
+        //a cada 2 palavras, as próximas 2 palavras têm a cor diferente.
+        if (Math.floor(i / 2) % 2 === 1) {
+            wordSpan = wordSpan.replace('<span>', '<span class="blue original-blue">');
+        }
+
+        formattedText += wordSpan;
     }
+
     textElement.innerHTML = formattedText;
 
     textElement.addEventListener('click', (event) => {
         if (event.target.tagName === 'SPAN') {
-            if (event.target.classList.toggle('red')) {
-                event.target.innerText = '☒';
-            } else {
+            if (event.target.classList.contains('red')) {
+                event.target.classList.remove('red');
                 event.target.innerText = '☐';
+
+                //reaplica a classe se a palavra tiver original.
+                if (event.target.classList.contains('original-blue')) {
+                    event.target.classList.add('blue');
+                }
+            } else {
+                event.target.classList.remove('blue'); //remove a classe se presente.
+                event.target.classList.add('red');
+                event.target.innerText = '☒';
             }
         }
     });
