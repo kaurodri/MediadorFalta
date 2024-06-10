0705cd92c347f060cf9352aa17446f5dd8d1b053
HieA1lpha <engkauanrodrigues@gmail.com>
2024-06-10 15:26:04 -0300

    2024-06-10

---
+++ v2.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let texto = "☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐ ☐☐";
+    let words = texto.split(' ');
+    const textElement = document.getElementById('text');
+
+    //recria o conteúdo com <span> e adiciona quebra de linha a cada quatro palavras.
+    let formattedText = '';
+    for (let i = 0; i < words.length; i++) {
+        let wordSpan = `<span>${words[i]}</span>`;
+        if ((i + 1) % 2 === 0 && i !== words.length - 1) {
+            wordSpan += 'ㅤ';
+        } if ((i + 1) % 4 === 0 && i !== words.length - 1) {
+            wordSpan += '<br>';
+        } else {
+            wordSpan += ' ';
+        }
+
+        //a cada 2 palavras, as próximas 2 palavras têm a cor diferente.
+        if (i % 2 === 1) {
+            wordSpan = wordSpan.replace('<span>', '<span class="blue original-blue">');
+        }
+
+        formattedText += wordSpan;
+    }
+
+    textElement.innerHTML = formattedText;
+
+    textElement.addEventListener('click', (event) => {
+        if (event.target.tagName === 'SPAN') {
+            if (event.target.classList.contains('red')) {
+                event.target.classList.remove('red');
+                event.target.innerText = '☐☐';
+
+                //reaplica a classe se a palavra tiver original.
+                if (event.target.classList.contains('original-blue')) {
+                    event.target.classList.add('blue');
+                }
+            } else {
+                event.target.classList.remove('blue'); //remove a classe se presente.
+                event.target.classList.add('red');
+                event.target.innerText = '☒☒';
+            }
+        }
+    });
+});
